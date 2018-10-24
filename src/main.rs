a4289265a6275374dc5f5a49ca597c681683298b
Arthur LEMAIRE <arthur.lemaire@epitech.com>
2018-10-24 11:08:35 +0200

    Added command line arguments hnadling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn print_usage() -> () {
+    println!("USAGE:\n\t./fun_evalexpr expr\n\nDESCRIPTION:\n\texpr\tmathematical expression to be evaluated")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    match args.len() {
+        2 => {
+            let exp = &args[1];
+            println!("Expression to evaluate : {}", exp);
+        },
+        //wrong argument number case
+        _ => {
+            print_usage();
+        }
+    }
 }
